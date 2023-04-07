8369e35e213cfd4c60517bed439cf13f4b0d38b0
Slicin <25377272+slicin@users.noreply.github.com>
2023-04-07 19:50:15 -0400

    Added gui

--- rps.js
+++ rps.js
@@ -23,7 +23,7 @@ function getComputerChoice() {
     return choiceString;
 }
 
-function playRound(playerSelection, computerSelection) {
+function determineWinner(playerSelection, computerSelection) {
     const playerSelectionLower = playerSelection.toLowerCase();
     const computerSelectionLower = computerSelection.toLowerCase();
 
@@ -79,48 +79,80 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
-function game() {
-    var playerScore = 0;
-    var computerScore = 0;
-    var round = 1;
+function playRound(e) {
+    round++;
+    var playerSelection = e.target.innerHTML.toLowerCase();
+    var computerSelection = getComputerChoice();
+    var result = determineWinner(playerSelection, computerSelection);
 
-    while (round <= 5) {
-        console.log("Round: " + round + "/5");
-        var playerSelection = prompt("Enter an option (rock, paper, or scissors):")
-        var computerSelection = getComputerChoice();
-        var result = playRound(playerSelection, computerSelection);
-
-        switch (result) {
-            case "Win":
-                playerScore++;
-                console.log("You won!");
-                break;
-            case "Lose":
-                computerScore++;
-                console.log("You lost.");
-                break;
-            case "Draw":
-                console.log("Draw!");
-                break;
-        }
-        console.log("You selected " + playerSelection);
-        console.log("Computer selected " + computerSelection);
-        console.log("Player score: " + playerScore);
-        console.log("Computer score: " + computerScore);
-        console.log("");
-        round++;
+    if (result === "Win") {
+        playerScore++;
+    }
+    else if (result === "Lose") {
+        computerScore++;
     }
 
-    console.log("Game Over.");
-    if (playerScore === computerScore) {
-        console.log("It was a draw!")
-    }
-    else if (playerScore > computerScore) {
-        console.log("You win!!!");
-    }
-    else {
-        console.log("You lose.");
+    updateScores();
+
+    if (round > 5) {
+        console.log("Game over");
+        endGame();
     }
 }
 
-game();
+function updateScores() {
+    playerScoreElement.innerHTML = "You: " + playerScore;
+    computerScoreElement.innerHTML = "Computer: " + computerScore;
+    roundNumber.innerHTML = "Round " + round;
+}
+
+function endGame() {
+    buttonRock.disabled = true;
+    buttonPaper.disabled = true;
+    buttonScissors.disabled = true;
+
+    var resultMessage = "";
+    if (playerScore > computerScore) {
+        resultMessage = "You win!";
+    }
+    else if (playerScore === computerScore) {
+        resultMessage = "It's a tie!";
+    }
+    else {
+        resultMessage = "You lose.";
+    }
+
+    roundNumber.innerHTML = resultMessage;
+    buttonReset.disabled = false;
+}
+
+function resetGame() {
+    buttonRock.disabled = false;
+    buttonPaper.disabled = false;
+    buttonScissors.disabled = false;
+    buttonReset.disabled = true;
+    round = 1;
+    playerScore = 0;
+    computerScore = 0;
+    updateScores();
+}
+
+// var round = 1;
+// var playerScore = 0;
+// var computerScore = 0;
+
+const playerScoreElement = document.getElementById("score-player");
+const computerScoreElement = document.getElementById("score-computer");
+
+const roundNumber = document.getElementById("round-number");
+const buttonRock = document.getElementById("button-rock");
+const buttonPaper = document.getElementById("button-paper");
+const buttonScissors = document.getElementById("button-scissors");
+const buttonReset = document.getElementById("button-reset");
+
+buttonRock.addEventListener("mouseup", playRound);
+buttonPaper.addEventListener("mouseup", playRound);
+buttonScissors.addEventListener("mouseup", playRound);
+buttonReset.addEventListener("mouseup", resetGame);
+
+resetGame();
